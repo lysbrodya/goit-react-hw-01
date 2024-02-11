b7ee7c5538ef0dafbef35c31be54faaebb21bdb3
lysbrodya <lysbrodya@gmail.com>
2024-02-11 12:27:18 +0200

    dop styles

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,9 +4,9 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img src={avatar} alt="Avatar" width="48" />
-      <p>{name}</p>
-      <p className={clsx(isOnline ? css.isOnline : css.isOfline)}>
-        {isOnline ? "isOnline" : "isOfline"}
+      <p className={css.friendName}>{name}</p>
+      <p className={clsx(css.status, isOnline ? css.isOnline : css.isOfline)}>
+        {isOnline ? "Online" : "Ofline"}
       </p>
       {/* {isOnline ? <p>isOnline</p> : <p>isOfline</p>} */}
     </div>
